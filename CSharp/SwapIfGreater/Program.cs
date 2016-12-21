b353a5196c712722f639c11fbe743b27cc28d9ec
NikolaLefterov <nikola.lefterov@hotmail.com>
2016-12-22 01:36:19 +0200

    Add files via upload

---
+++ CSharp/SwapIfGreater/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            if(a> b)
+            {
+                double tmp = a;
+                a = b;
+                b = tmp;
+                Console.WriteLine(a + " " + b);
+
+            }
+            else
+            {
+                Console.WriteLine(a + " " + b);
+
+            }
+            
+        }
+    }
+}
