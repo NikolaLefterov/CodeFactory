8c79994bc6e3dcb9cf9b631c5dd16f72244bc052
NikolaLefterov <nikola.lefterov@hotmail.com>
2016-12-22 01:17:27 +0200

    Add files via upload

---
+++ CSharp/NBith/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBith
+{
+    class Program
+    {
+
+        static void Main()
+        {
+          
+            long number = int.Parse(Console.ReadLine());
+            
+            int position = int.Parse(Console.ReadLine());
+
+            long moveBit = number >> position;
+           long foundBit = moveBit & 1;
+
+            bool isOne = foundBit == 1;
+            if(foundBit == 1)
+            {
+                Console.WriteLine(1);
+            }
+            else
+            Console.WriteLine(0);
+            
+            
+        }
+    }
+}
+
