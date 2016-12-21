8dc5a14602c67e6997f7dcceb16360b3d8196a93
NikolaLefterov <nikola.lefterov@hotmail.com>
2016-12-22 01:19:46 +0200

    Add files via upload

---
+++ CSharp/ThirdDigit/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdDig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+            num = (num / 100) % 10;
+            if(num == 7)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false "+num);
+            }
+        }
+    }
+}
