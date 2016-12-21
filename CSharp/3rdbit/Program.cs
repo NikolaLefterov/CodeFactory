af78773bc7029e041eb36e8ae9172da592d188d7
NikolaLefterov <nikola.lefterov@hotmail.com>
2016-12-22 01:08:06 +0200

    Add files via upload

---
+++ CSharp/3rdbit/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3rdbit
+{
+    class Program
+    {
+        static void Main()
+        {
+           
+            int number = int.Parse(Console.ReadLine());
+
+            int checkBit = 1 << 3;
+            int thirdBit = number & checkBit;
+
+            if (thirdBit == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                Console.WriteLine(1);
+            }
+        }
+    }
+}
