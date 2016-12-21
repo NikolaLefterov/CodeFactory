3d5978b4676cde6115669c684fef1df121e7bfb0
NikolaLefterov <nikola.lefterov@hotmail.com>
2016-12-22 01:16:42 +0200

    Add files via upload

---
+++ CSharp/MoonGravity/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoonGravity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double weight = Convert.ToDouble(Console.ReadLine());
+            weight = (weight * 17) / 100;
+            Console.WriteLine(weight.ToString("F3"));
+           
+            
+        }
+    }
+}
