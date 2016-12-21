146f1aff52421a7365582adbeef5139927c2ab9b
NikolaLefterov <nikola.lefterov@hotmail.com>
2016-12-22 01:57:02 +0200

    Add files via upload

---
+++ CSharp/MaxKsum/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxSumK
+{
+    class MaxSumK
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+           var list = arr.ToList();
+            list.Sort();
+            list.Reverse();
+            arr = list.ToArray();
+
+            for (int i = 0; i < k; i++)
+            {
+                sum += arr[i];
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
