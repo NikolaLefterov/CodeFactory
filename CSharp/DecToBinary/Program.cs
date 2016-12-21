f7fb26b1f6c3519fa824a6e07bbf944d3c8cc291
NikolaLefterov <nikola.lefterov@hotmail.com>
2016-12-22 01:39:19 +0200

    Add files via upload

---
+++ CSharp/DecToBinary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dectobinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string binary = "";
+            while(n>=1)
+            {
+                binary = (n % 2) + binary;
+                n = n / 2;
+            }
+            Console.WriteLine(binary);
+        }
+    }
+}
