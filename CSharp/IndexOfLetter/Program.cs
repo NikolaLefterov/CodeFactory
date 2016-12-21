04f4de6e2d3f5fbd548de65a81ea6552fbbbed2c
NikolaLefterov <nikola.lefterov@hotmail.com>
2016-12-22 01:55:31 +0200

    Add files via upload

---
+++ CSharp/IndexOfLetter/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace indexofaletter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            string input = Console.ReadLine();
+            int[] n = new int[input.Length];
+            for (int i = 0; i < input.Length; i++)
+            {
+                n[i] = Convert.ToInt32(input[i]);
+                Console.WriteLine(n[i]-97);
+            }
+           
+        }
+    }
+}
