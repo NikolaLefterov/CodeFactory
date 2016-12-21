f77f6bdff19dd6a157707754f801f3a835246433
NikolaLefterov <nikola.lefterov@hotmail.com>
2016-12-22 01:15:06 +0200

    Add files via upload

---
+++ CSharp/Devided7and5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Devided_7_and_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num % 5 == 0 && num % 7 == 0)
+            {
+                Console.WriteLine("true " +num);
+            }
+            else
+                Console.WriteLine("false " + num);
+
+        }
+    }
+}
