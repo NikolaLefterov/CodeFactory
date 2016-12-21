36a9e8fe93312ec8b97afba3e6fc32e35a658d55
NikolaLefterov <nikola.lefterov@hotmail.com>
2016-12-22 01:28:58 +0200

    Add files via upload

---
+++ CSharp/BubbleSort/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bublesort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            // the code that you want to measure comes here
+           
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            Random rnd = new Random();
+            int temp = 0;
+            
+            for (int i = 0; i < n; i++)
+            {
+
+                arr[i] = rnd.Next(-65000,65000);
+            }
+            for (int write = 0; write <= arr.Length-1; write++)
+            {
+                for (int sort = write+1; sort <=arr.Length - 1; sort++)
+                {
+                    if (arr[write] > arr[sort])
+                    {
+                        temp = arr[write];
+                        arr[write] = arr[sort];
+                        arr[sort] = temp;
+                    }
+                }
+            }
+
+         //   for (int i = 0; i < arr.Length; i++)
+              //  Console.WriteLine(arr[i]);
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine(elapsedMs);
+        }
+    }
+}
