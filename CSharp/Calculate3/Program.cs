01d3a9bae853c665b9bbde826cd5beaf1d01f41b
NikolaLefterov <nikola.lefterov@hotmail.com>
2016-12-22 01:46:21 +0200

    Add files via upload

---
+++ CSharp/Calculate3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Calculate3
+{
+    class Program
+    {
+        public static BigInteger factorial(int x)
+        {
+            BigInteger fac = x;
+            for (int i = 1; i <= x - 1; i++)
+            {
+                fac = fac * i;
+            }
+            return fac;
+        }
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+            Console.WriteLine((factorial(n))/((factorial(k))*(factorial(n- k))));
+        }
+    }
+}
