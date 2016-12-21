0d090aed8c37b03676481af24a53b961f82c6dcb
NikolaLefterov <nikola.lefterov@hotmail.com>
2016-12-22 01:56:33 +0200

    Add files via upload

---
+++ CSharp/MaxIncreasingSequence/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maxincreasesequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            int []n = new int[length];
+            int counter = 1;
+            int result = 1;
+            for (int i = 0; i < length; i++)
+            {
+                n[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < length-1; i++)
+            {
+                if (n[i + 1] - n[i] >= 1)
+                {
+                    counter++;
+                    if (counter > result)
+                    {
+                        result = counter;
+                    }
+                }
+                else
+                    counter = 1;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
