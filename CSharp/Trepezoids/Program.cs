961f85272ee78daca8b5e84f77dabd884fc1fdd1
NikolaLefterov <nikola.lefterov@hotmail.com>
2016-12-22 01:21:36 +0200

    Add files via upload

---
+++ CSharp/Trepezoids/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trapezoids
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, h;
+            a = double.Parse(Console.ReadLine());
+            b = double.Parse(Console.ReadLine());
+            h = double.Parse(Console.ReadLine());
+            double area = ((a +b) / 2) * h;
+            Console.WriteLine(area.ToString("F7"));
+        }
+    }
+}
