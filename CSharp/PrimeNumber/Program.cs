5c82e0c8ef793c0be723055e18a6e48718cd5f0f
NikolaLefterov <nikola.lefterov@hotmail.com>
2016-12-22 01:18:35 +0200

    Add files via upload

---
+++ CSharp/PrimeNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n = int.Parse(Console.ReadLine());
+            int counter = 0;
+           for (int i = 1; i <= n;i++)
+            {
+                if (n % i == 0)
+                {
+                    counter++;
+                }
+            }
+            if (counter == 2)
+            {
+
+                Console.WriteLine("true");
+            }
+            else
+                Console.WriteLine("false");
+        }
+    }
+}
