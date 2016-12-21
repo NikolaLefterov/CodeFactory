fd200d4f4e4f075863726af86a20f6d925909575
NikolaLefterov <nikola.lefterov@hotmail.com>
2016-12-22 01:19:12 +0200

    Add files via upload

---
+++ CSharp/Rectagle/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double weight = Convert.ToDouble(Console.ReadLine());
+            double height = Convert.ToDouble(Console.ReadLine());
+            double area = weight * height;
+            double perimeter = (2 * weight) + (2 * height);
+            Console.WriteLine(area.ToString("F2")+" "+perimeter.ToString("F2"));
+            
+
+        }
+    }
+}
