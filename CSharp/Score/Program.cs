08588392c1c441105a590dfab5d7e36673474219
NikolaLefterov <nikola.lefterov@hotmail.com>
2016-12-22 01:44:11 +0200

    Add files via upload

---
+++ CSharp/Score/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace score
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            if(input >= 1 && input <= 3)
+            {
+                Console.WriteLine(input*10);
+            }
+            else if(input >=4 && input <= 6)
+            {
+                Console.WriteLine(input*100);
+            }
+            else if(input >=7 && input <= 9)
+            {
+                Console.WriteLine(input*1000);
+            }
+            else
+                Console.WriteLine("invalid score");
+        }
+    }
+}
