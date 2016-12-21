58b4feca7e3a756091b9de198268b126d6a55669
NikolaLefterov <nikola.lefterov@hotmail.com>
2016-12-22 01:40:59 +0200

    Add files via upload

---
+++ CSharp/GCD/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gcd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tokens = Console.ReadLine().Split();
+
+           
+            int a = int.Parse(tokens[0]);
+
+            
+            int b = int.Parse(tokens[1]);
+            int r;
+            while (b != 0)
+            {
+                r = a % b;
+                a = b;
+                b = r;
+            }
+            Console.WriteLine(a);
+        }
+    }
+}
