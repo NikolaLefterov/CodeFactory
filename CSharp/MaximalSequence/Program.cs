abad14f6e1e554c32bb621352e3ceb46cdd76d58
NikolaLefterov <nikola.lefterov@hotmail.com>
2016-12-22 01:56:03 +0200

    Add files via upload

---
+++ CSharp/MaximalSequence/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maximalSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int[] n = new int[N];
+            int[] tmp = new int[N];
+            int j = 0;
+            for (int i = 0; i < N; i++)
+            {
+                n[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int counter = 0;
+            for (int i = 0; i < N-1; i++)
+            {
+                if(n[i] == n[i + 1]) {
+                    counter++;
+                }
+                else {
+                    counter++;
+                    tmp[j] = counter;
+                    j++;
+                    counter = 0;
+                }
+                    
+
+            }
+            counter++;
+            tmp[j] = counter;
+            Console.WriteLine(tmp.Max());
+        }
+    }
+}
