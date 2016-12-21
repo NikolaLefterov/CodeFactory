d625e54b4d7c5ecfdd5570709824a10aa51408f2
NikolaLefterov <nikola.lefterov@hotmail.com>
2016-12-22 01:49:04 +0200

    Add files via upload

---
+++ CSharp/NotDevBy7and3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotDevby7and3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 1;i<= n; i++)
+            {
+                if(i%3 == 0 || i%7 == 0)
+                {
+                    
+                }
+                else
+                    Console.Write(i+ " ");
+            }
+        }
+    }
+}
