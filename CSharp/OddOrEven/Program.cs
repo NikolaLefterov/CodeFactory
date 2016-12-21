e543d78c1370a55182b2653fbe52279f5a6db04f
NikolaLefterov <nikola.lefterov@hotmail.com>
2016-12-22 01:10:44 +0200

    Add files via upload

---
+++ CSharp/OddOrEven/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            num = Convert.ToInt32(Console.ReadLine());
+            if(num % 2 == 0)
+            {
+                Console.WriteLine("even " + num );
+            }
+            else
+            {
+                Console.WriteLine("odd " + num);
+            }
+        }
+    }
+}
