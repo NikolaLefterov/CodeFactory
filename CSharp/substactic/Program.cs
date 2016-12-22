7e23b47216bfcc04061b3be4414dc539b7847c31
NikolaLefterov <nikola.lefterov@hotmail.com>
2016-12-22 02:25:13 +0200

    Add files via upload

---
+++ CSharp/substactic/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace substacting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            string substring = Console.ReadLine();
+            string input = Console.ReadLine();
+            int result;
+            for (int i = 0; ; counter++)
+            {
+                result = input.IndexOf(substring, i, StringComparison.OrdinalIgnoreCase);
+                if (result < 0)
+                    break;
+                i = result + substring.Length;
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
