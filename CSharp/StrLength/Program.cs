b1569440b15b22db8a32f0b7a07b10a8c30b0be2
NikolaLefterov <nikola.lefterov@hotmail.com>
2016-12-22 02:24:47 +0200

    Add files via upload

---
+++ CSharp/StrLength/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace strlength
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder input = new StringBuilder(Console.ReadLine());
+            for (int i = input.Length; i < 20; i++)
+            {
+                input.Append("*");
+            }
+            Console.WriteLine(input);
+        }
+    }
+}
