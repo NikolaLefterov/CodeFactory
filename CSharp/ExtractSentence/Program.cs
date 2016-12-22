1f492d6b8be6d4344f5e2b0551efc73361b36e47
NikolaLefterov <nikola.lefterov@hotmail.com>
2016-12-22 02:23:18 +0200

    Add files via upload

---
+++ CSharp/ExtractSentence/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace extactsentece
+{
+   
+    class Program
+    {
+       static void  Extract(string str, string keyword)
+        {
+            string[] arr = str.Split('.');
+            string answer = string.Empty;
+
+            foreach (string sentence in arr)
+            {
+                
+                string[] words = sentence.Split(new char[] { ' ', ',' });
+                if (words.Contains(keyword))
+                {
+                    answer = sentence;
+                    Console.Write(answer+".");
+                }
+              
+            }
+            Console.WriteLine();
+
+        }
+        static void Main(string[] args)
+        {
+            string keyword = Console.ReadLine();
+            string input = Console.ReadLine();
+            Extract(input, keyword);
+        }
+    }
+}
