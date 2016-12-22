0b7c3b5192acadcf4854ce3cd8683e18e87721f7
NikolaLefterov <nikola.lefterov@hotmail.com>
2016-12-22 02:16:53 +0200

    Add files via upload

---
+++ CSharp/GetLargesttNum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+
+class GetLargestNumber
+{
+    
+    static void Main()
+    {
+        GetMax(ReadNumbers());
+    }
+
+    static string ReadNumbers()
+    {
+        string numbers = Console.ReadLine();
+        return numbers;
+    }
+    
+    static void GetMax(string inputNumbers)
+    {
+        int[] arrayNumbers = inputNumbers.Split(' ').Select(n => Convert.ToInt32(n)).ToArray();
+        Console.WriteLine(arrayNumbers.Max());
+    }
+}
