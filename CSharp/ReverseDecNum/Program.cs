91d396d1f5f1d9338d71b334dfd2ee899176e46b
NikolaLefterov <nikola.lefterov@hotmail.com>
2016-12-22 02:13:53 +0200

    Add files via upload

---
+++ CSharp/ReverseDecNum/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main()
+        {
+            string n = Console.ReadLine();
+            Console.WriteLine(ReverseDecNum(n));
+        }
+
+        
+        static string ReverseDecNum(string number)
+        {
+            char[] arrNumber = number.ToCharArray();
+            Array.Reverse(arrNumber);
+            string numberString = new string(arrNumber);
+
+            return numberString;
+        }
+    }
+}
