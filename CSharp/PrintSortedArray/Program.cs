fdff55dd4660b6cdbe239bd7a3e0bef70c556c8b
NikolaLefterov <nikola.lefterov@hotmail.com>
2016-12-22 02:15:47 +0200

    Add files via upload

---
+++ CSharp/PrintSortedArray/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+class SortArr
+{
+    
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] array = fillArr(n);
+        int[] sortedArray = SortArray(array);
+        PrintSortedArr(sortedArray,n);
+    }
+
+   
+    
+
+    static int[] fillArr(int n)
+    {
+        
+        string[] tokens = new string[n];
+        tokens = Console.ReadLine().Split();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+
+            arr[i] = int.Parse(tokens[i]);
+        }
+        return arr;
+
+    }
+    static int[] SortArray(int[] array)
+    {
+        Array.Sort(array);
+        return array;
+    }
+
+   
+    static void PrintSortedArr(int[] sortArray , int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(sortArray[i]+ " ");
+        }
+    }
+}
