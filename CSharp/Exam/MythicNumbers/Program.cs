913d49d870b983ad40b031644bdb5ff11c94b036
NikolaLefterov <nikola.lefterov@hotmail.com>
2016-12-22 02:05:50 +0200

    Add files via upload

---
+++ CSharp/Exam/MythicNumbers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            uint n = uint.Parse(Console.ReadLine());
+            uint firstDigit = 0, secondDigit = 0, thirdDigit = 0;
+            thirdDigit = n % 10;
+            n = n / 10;
+            secondDigit = n % 10;
+            n = n / 10;
+            firstDigit = n % 10;
+            double product = 0;
+            if(thirdDigit == 0)
+            {
+                product = (double)Math.Max((firstDigit*secondDigit), secondDigit);
+                Console.WriteLine(product.ToString("F2"));
+            }
+            else if(thirdDigit >0 && thirdDigit <= 5)
+            {
+                product = (double)Math.Max((firstDigit*secondDigit), secondDigit) / (double)thirdDigit;
+                Console.WriteLine(product.ToString("F2"));
+            }
+            else if(thirdDigit> 5)
+            {
+                product = (double)(firstDigit + secondDigit) *(double) thirdDigit;
+                Console.WriteLine(product.ToString("F2"));
+            }
+        }
+    }
+}
