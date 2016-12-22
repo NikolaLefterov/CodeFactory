1f501b91c029a43af2d40dfedc62e4d3a97fbf18
NikolaLefterov <nikola.lefterov@hotmail.com>
2016-12-22 02:16:21 +0200

    Add files via upload

---
+++ CSharp/NFactorial/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Numerics;
+
+
+class Factorial
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        Nfac(n);
+    }
+
+    static void Nfac(int number)
+    {
+        BigInteger factorial = 1;
+
+        if (number < 0)
+        {
+            return;
+        }
+        
+        else
+        {
+            for (int i = 1; i <= number; i++)
+            {
+                factorial *= i;
+            }
+
+            Console.WriteLine(factorial);
+            return;
+        }
+    }
+}
