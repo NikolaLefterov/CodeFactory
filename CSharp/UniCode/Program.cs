b39cc9b509949b165a973c5c65a6245fd0e697f1
NikolaLefterov <nikola.lefterov@hotmail.com>
2016-12-22 02:22:52 +0200

    Add files via upload

---
+++ CSharp/UniCode/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            StringBuilder unicode = new StringBuilder();
+            foreach (char ch in input)
+
+            {
+                unicode.AppendFormat("\\u{0:X4}", (int)ch);
+            }
+
+            Console.WriteLine(unicode.ToString());
+        }
+    }
+}
